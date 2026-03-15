49577244f8bb2fcb48c37bc99c8384f48d9c1eeb
Rico Chandra <rico.chandra@yale.edu>
2026-03-14 23:36:32 -0400

    added base schemas using sqlalchemy for ORM

---
+++ backend/app/models/trip.py
@@ -0,0 +1,19 @@
+from datetime import date, datetime
+
+from sqlalchemy import Date, DateTime, ForeignKey, Integer, String
+from sqlalchemy.orm import Mapped, mapped_column
+
+from app.db.database import Base
+
+class Trip(Base):
+    __tablename__ = "trips"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
+    name: Mapped[str] = mapped_column(String(150), nullable=False)
+    destination_name: Mapped[str] = mapped_column(String(150), nullable=False)
+
+    start_date: Mapped[date] = mapped_column(Date, nullable=False)
+    end_date: Mapped[date] = mapped_column(Date, nullable=False)
+
+    created_by_user_id: Mapped[int] = mapped_column(ForeignKey("users.id"), nullable=False)
+    created_at: Mapped[datetime] = mapped_column(DateTime, default=datetime.utcnow, nullable=False)
