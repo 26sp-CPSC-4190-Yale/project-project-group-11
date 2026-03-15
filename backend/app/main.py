7b5f1aaba125dd91ccaa2a54c3a426bb886a80ce
Rico Chandra <rico.chandra@yale.edu>
2026-03-14 23:38:18 -0400

    create db on startup if not existing

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
 from app.routers import health
+from app.db.database import Base, engine
+from app.models import User, Trip, TripMember, FlightSearch
 
 app = FastAPI(title="My API")
 
@@ -8,3 +10,7 @@ app.include_router(health.router)
 @app.get("/")
 async def root():
     return {"message": "API is running"}
+
+@app.on_event("startup") # on startup make the db if not already existed.
+def on_startup():
+    Base.metadata.create_all(bind=engine)
