e59f02c2586ad2003d5c69cf0da4e1f52e32e391
Yejun <yejunyun@Yejuns-MBP.wireless.yale.internal>
2026-04-05 16:59:37 -0400

    update

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -3,3 +3,4 @@ from app.models.trip import Trip
 from app.models.trip_member import TripMember
 from app.models.flight_search import FlightSearch
 from app.models.flight import Flight
+from app.models.itinerary_item import ItineraryItem
